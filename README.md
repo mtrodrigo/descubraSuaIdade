0a11546fdc12417b0218847b5fb835525a3f0e97
Rodrigo Marques Tavares <rodrigour@gmail.com>
2025-02-04 10:46:08 -0300

    Projeto finalizado

---
+++ README.md
@@ -0,0 +1,40 @@
+# Descubra Sua Idade
+
+## Descrição do Projeto
+Este projeto é uma aplicação React que calcula a sua idade com base na data de nascimento fornecida. É uma ferramenta simples e intuitiva para descobrir quantos anos você tem.
+
+## Instalação
+
+Para rodar este projeto localmente, siga os passos abaixo:
+
+1. Clone o repositório:
+  ```bash
+  git clone <URL_DO_REPOSITORIO>
+  ```
+2. Navegue até o diretório do projeto:
+  ```bash
+  cd descubraSuaIdade
+  ```
+3. Instale as dependências:
+  ```bash
+  npm install
+  ```
+4. Inicie a aplicação:
+  ```bash
+  npm start
+  ```
+
+## Linguagens Utilizadas
+
+```mermaid
+pie
+   title Linguagens Utilizadas
+   "JavaScript": 70
+   "HTML": 15
+   "CSS": 15
+```
+
+## Contato
+
+- WhatsApp: [35984061841](https://wa.me/35984061841)
+- LinkedIn: [Rodrigo Marques Tavares](https://www.linkedin.com/in/rodrigo-marques-tavares-9482b4226/)
---
+++ src/App.tsx
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [nome, setNome] = useState<string>("")
+  const [ano, setAno] = useState<number>()
+  const [idade, setIdade] = useState<number>()
+  const [nomeExibir, setNomeExibir] = useState<string>("")
+    
+  const dataAtual = new Date()
+  const anoAtual = dataAtual.getFullYear()
+ 
+  const gerarIdade = () => {
+    const idadeGerada = ano ? anoAtual - ano : 0;
+    setIdade(idadeGerada)
+    setNomeExibir(nome)
+  }
+
+  return (
+    <main className='main'>
+      <section className='container'>
+        <h1>Descubra sua idade</h1>
+        <div className='app_container'>
+          <div className='input_container'>
+          <label>Qual seu nome?</label>
+          <input 
+          value={nome}
+          onChange={(e) => setNome(e.target.value)}
+          placeholder='Digite seu nome'
+          />
+          </div>
+          <div className='input_container'>
+          <label>Qual ano você nasceu?</label>
+          <input
+          value={ano}
+          onChange={(e) => setAno(Number(e.target.value))} 
+          type="text"
+          placeholder='Digite ano que você nasceu'
+          />
+          </div>
+          <div className='btn_container'>
+            <button onClick={gerarIdade}>Descobrir idade</button>
+          </div>
+        </div>
+        {nomeExibir !== "" && 
+          <div>
+            <h3>{nomeExibir}</h3>
+            <h4>Sua idade é: {idade}</h4>
+          </div>
+        }
+      </section>
+    </main>
+  )
+}
+
+export default App
